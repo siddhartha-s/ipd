827c40440d56be9ef8127e2bb25982c00a06d7db
Jesse Tov <jesse.tov@gmail.com>
2015-10-20 10:40:54 -0500

    outline for lecture today

---
+++ lectures/2015-10-20/generative.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname generative) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+  Problems:
+
+    - Newton's method
+
+    - merge sort
+
+    - Graph problems/algorithms:
+        - BFS
+        - DFS
+        - SSSP (Dykstra's algorithm?)
+        - Some MSP algorithm?
+|#
