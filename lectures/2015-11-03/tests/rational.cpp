2f1b0864d593ba2e3848c733eec80664f41d822e
Jesse Tov <jesse.tov@gmail.com>
2015-11-03 14:02:24 -0600

    added empty test file for rationals

---
+++ lectures/2015-11-03/tests/rational.cpp
@@ -0,0 +1,13 @@
+#include "rational/rational.hpp"
+#include <UnitTest++/UnitTest++.h>
+
+namespace rational
+{
+
+} // namespace rational
+
+int
+main(int, const char*[])
+{
+    return UnitTest::RunAllTests();
+}
