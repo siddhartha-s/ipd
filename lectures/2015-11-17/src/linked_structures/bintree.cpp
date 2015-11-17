0203fe85f62d2446a0e22aaf69f0a91532e481e2
Jesse Tov <jesse.tov@gmail.com>
2015-11-17 10:00:00 -0600

    lecture prep code for today

---
+++ lectures/2015-11-17/src/linked_structures/bintree.cpp
@@ -0,0 +1,1 @@
+#include "bintree.hpp"
---
+++ lectures/2015-11-17/tests/linked_structures.cpp
@@ -0,0 +1,18 @@
+#include <UnitTest++/UnitTest++.h>
+
+#include <string>
+
+namespace linked_structures
+{
+
+TEST(Create)
+{
+}
+
+}  // namespace linked_structures
+
+int
+main(int, const char* [])
+{
+    return UnitTest::RunAllTests();
+}
