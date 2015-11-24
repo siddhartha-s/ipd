c0c73aae6ee2bf5b2a159525c0c5327e1608fff9
Jesse Tov <jesse.tov@gmail.com>
2015-11-24 16:28:26 -0600

    some sampleable additions

--- lectures/2015-11-24/src/graphics/sampleable.cpp
+++ lectures/2015-11-24/src/graphics/sampleable.cpp
@@ -1,16 +1,58 @@
 #include "sampleable.hpp"
 
+#include "gsl/gsl.h"
+
 namespace graphics
 {
+
+using raster::fcolor;
 
 void sample(const sampleable& scene, raster::raster& target)
 {
     for (int x = 0; x < target.width(); ++x) {
         for (int y = 0; y < target.height(); ++y) {
-            double xd = x, yd = y;
-            target[{x, y}] = scene.color_at({xd, yd});
+            target[{x, y}] = scene.color_at({x + 0.5, y + 0.5});
         }
     }
+}
+
+rectangle::rectangle(coord p1, coord p2, color c)
+    : box_{p1, p2}, color_{c}
+{ }
+
+fcolor rectangle::color_at(coord p) const
+{
+    if (box_.within(p)) {
+        return color_;
+    } else {
+        return color::TRANSPARENT;
+    }
+}
+
+rectangle::bbox rectangle::get_bounding_box() const
+{
+    return box_;
+}
+
+circle::circle(coord center, double radius, fcolor color)
+    : center_{center}, radius_{radius}, color_{color}
+{
+    Expects(radius > 0);
+}
+
+fcolor circle::color_at(coord p) const
+{
+    if (distance(p, center_) <= radius_) {
+        return color_;
+    } else {
+        return fcolor::TRANSPARENT;
+    }
+}
+
+rectangle::bbox circle::get_bounding_box() const
+{
+    return {{center_.x - radius_, center_.y - radius_},
+            {center_.x + radius_, center_.y + radius_}};
 }
 
 } // namespace graphics
--- lectures/2015-11-24/src/graphics/sampleable.hpp
+++ lectures/2015-11-24/src/graphics/sampleable.hpp
@@ -13,9 +13,38 @@ class sampleable : public raster::boundable<double>
 {
 public:
     using coord = raster::point<double>;
+    using bbox  = raster::bounding_box<double>;
+    using color = raster::fcolor;
     using ptr   = std::shared_ptr<sampleable>;
 
-    virtual raster::fcolor color_at(coord) const = 0;
+    virtual color color_at(coord) const = 0;
+};
+
+class rectangle : public sampleable
+{
+public:
+    rectangle(coord, coord, color);
+
+    virtual color color_at(coord) const override;
+    virtual bbox get_bounding_box() const override;
+
+private:
+    bbox box_;
+    color color_;
+};
+
+class circle : public sampleable
+{
+public:
+    circle(coord, double radius, color);
+
+    virtual color color_at(coord) const override;
+    virtual bbox get_bounding_box() const override;
+
+private:
+    coord center_;
+    double radius_;
+    color color_;
 };
 
 void
