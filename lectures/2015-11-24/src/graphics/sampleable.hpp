b13bd76247ebdeb6dd08d9f5c68df1c16f6c92e7
Jesse Tov <jesse.tov@gmail.com>
2015-11-24 15:42:10 -0600

    added to sampleable

--- lectures/2015-11-24/src/graphics/sampleable.hpp
+++ lectures/2015-11-24/src/graphics/sampleable.hpp
@@ -4,6 +4,8 @@
 #include "raster/geometry.hpp"
 #include "raster/raster.hpp"
 
+#include <memory>
+
 namespace graphics
 {
 
@@ -11,6 +13,7 @@ class sampleable : public raster::boundable<double>
 {
 public:
     using coord = raster::point<double>;
+    using ptr   = std::shared_ptr<sampleable>;
 
     virtual raster::fcolor color_at(coord) const = 0;
 };
