72021c7ce2722c70e93a4f02ffa076e0c7ad4265
Jesse Tov <jesse.tov@gmail.com>
2015-12-01 14:11:13 -0600

    redacted for class

--- lectures/2015-11-24/tests/graphics.cpp
+++ lectures/2015-11-24/tests/graphics.cpp
@@ -28,34 +28,6 @@ TEST(SimpleRectangle)
     render(rect, "rectangle.ppm");
 }
 
-TEST(SimpleCircle)
-{
-    auto circ = circle({WIDTH/2, HEIGHT/2}, 150, fcolor{0, .6, 0});
-    render(circ, "circle.ppm");
-}
-
-TEST(SimpleOverlay)
-{
-    auto background = canvas({.5, .5, .5});
-    auto circ1 = circle({200, 100}, 150, fcolor{0, 1, 0, 0.4});
-    auto circ2 = circle({400, 100}, 150, fcolor{1, 0, 0, 0.4});
-    auto circ3 = circle({300, 300}, 150, fcolor{0, 0, 1, 0.4});
-
-    auto scene = overlay(circ1, overlay(circ2, overlay(circ3, background)));
-
-    render(scene, "overlay.ppm");
-}
-
-TEST(Affinity)
-{
-    auto rect = rectangle({WIDTH / 3, HEIGHT / 3},
-                          {2 * WIDTH / 3, 2 * HEIGHT / 3},
-                          fcolor{0.6, 0, 0});
-    auto trans = raster::affinity::rotation(.7);
-
-    render(transform(trans, rect), "affinity.ppm");
-}
-
 }  // namespace graphics
 
 int
