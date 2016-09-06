4cce4523bf95e2864eee743ab26aaf321c86b19e
Robby Findler <robby@racket-lang.org>
2016-09-05 19:06:51 -0500

    handin server instructions

---
+++ hw/01/setup-start-here.rkt
@@ -0,0 +1,41 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname getting-set-up) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+
+1) Install DrRacket version 6.6:
+
+   https://racket-lang.org
+
+2) Open this file in DrRacket and hit the "Run" button.
+   Make sure you see a 31 digit number appear in the
+   Interactions Window.
+
+3) Choose the "File | Install .plt file..." menu item.
+
+   Click "Web" in the top of the resulting dialog and
+   paste this into the dialog:
+
+http://users.eecs.northwestern.edu/~jesse/course/eecs495ipd-fa16/nwu-ipd-2016.plt
+
+   Hit OK and wait until the "Close" button is enabled.
+   Click it and restart DrRacket.
+
+4) You should see a new button in the DrRacket toolbar.
+   (Ignore it for now)
+
+5) Choose the "File | Manage IPD Handin Account" to
+   create a new account. Do NOT use your netid's real password!
+
+6) Each file you complete is handed in separately. When you
+   finish one, click that new button (the one ignored in
+   step 4) and make sure you submit it to the right assignment.
+   Submit one to each.
+
+7) check on the status of your submissions here:
+
+    https://penghu.eecs.northwestern.edu:8082/
+
+|#
+
+(expt 2 100)
