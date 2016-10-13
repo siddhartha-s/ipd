bd72f0fb59bb975acc61142e839d26c6bf839afd
Jesse A. Tov <jesse.tov@gmail.com>
2016-10-13 09:17:45 -0500

    BST cleanup.

--- lectures/lec17/src/Bst.h
+++ lectures/lec17/src/Bst.h
@@ -5,37 +5,46 @@
 
 namespace ipd {
 
+// A simple binary search tree using leaf-insertion (no balance).
 template<typename T>
 class Bst
 {
 public:
+    // Constructs an empty tree.
     Bst();
+    // Constructs a tree containing the given elements.
     Bst(std::initializer_list<T>);
 
+    // Is this tree empty?
     bool empty() const;
+    // Returns the number of elements in the tree.
     size_t size() const;
 
-    void insert(const T&);
-    void remove(const T&);
+    // Does the tree contain the given value?
     bool contains(const T&) const;
+    // Inserts an element into the tree, if absent.
+    void insert(const T&);
+    // Removes an element from the tree, if present.
+    void remove(const T&);
 
 private:
     struct node_;
     using ptr_ = std::unique_ptr<node_>;
 
-    struct node_
-    {
-        node_(const T& value)
-                : data(value), left(nullptr), right(nullptr), size(1) {}
-
-        T      data;
-        ptr_   left;
-        ptr_   right;
-        size_t size;
-    };
-
     ptr_   root_;
     size_t size_;
+};
+
+template<typename T>
+struct Bst<T>::node_
+{
+    node_(const T& value)
+            : data(value), left(nullptr), right(nullptr), size(1) {}
+
+    T      data;
+    ptr_   left;
+    ptr_   right;
+    size_t size;
 };
 
 template<typename T>
@@ -60,6 +69,20 @@ template<typename T>
 size_t Bst<T>::size() const
 {
     return size_;
+}
+
+template<typename T>
+bool Bst<T>::contains(const T& key) const
+{
+    node_* curr = &*root_;
+
+    while (curr != nullptr) {
+        if (key < curr->data) curr = &*curr->left;
+        else if (key > curr->data) curr = &*curr->right;
+        else return true;
+    }
+
+    return false;
 }
 
 template<typename T>
@@ -99,20 +122,6 @@ void Bst<T>::remove(const T& key)
             return;
         }
     }
-}
-
-template<typename T>
-bool Bst<T>::contains(const T& key) const
-{
-    node_* curr = &*root_;
-
-    while (curr != nullptr) {
-        if (key < curr->data) curr = &*curr->left;
-        else if (key > curr->data) curr = &*curr->right;
-        else return true;
-    }
-
-    return false;
 }
 
 }
