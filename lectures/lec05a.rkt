4a2a7197d9559250412db71f3c034a90b88e440f
Robby Findler <robby@racket-lang.org>
2017-10-01 15:25:23 -0500

    move lec05a to be a rkt file

--- lectures/lec05a.rkt
+++ lectures/lec05a.rkt
@@ -1,3 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname lec05a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
 mutually referential data definitions
 -------------------------------------
 
@@ -28,6 +32,7 @@ Thinking about these as parents who have children that themselves have
 children, etc.
 
 What is the data definition we get for these trees?
+|#
 
 ; a family-tree is:
 ;   (make-parent list-of-family-tree string number string)
@@ -37,13 +42,14 @@ What is the data definition we get for these trees?
 ;  - empty
 ;  - (cons family-tree list-of-family-tree)
 
-What happened here? Mututally referential data definitions!
+#|
+Note that these are mutually recursive data definitions!
 
 What do you think will happen to the functions? ...?
 
 Lets write out the example above as a descendant family tree.
-
 Starting from the bottom:
+|#
 
 (define gustav (make-parent '() "gustav" 1988 "brown"))
 (define adam (make-parent '() "adam" 1950 "yellow"))
@@ -59,14 +65,16 @@ Starting from the bottom:
                 	     1926
 			     "green"))
 
-Lets write a template for these functions:
+;; Here is the template for these functions:
 
+#;
 (define (ft-template ft)
   (lft-template (parent-children ft))
   (parent-name ft)
   (parent-date ft)
   (parent-eyes ft))
 
+#;
 (define (lft-template lft)
   (cond
    [(empty? lft) ..]
@@ -74,20 +82,27 @@ Lets write a template for these functions:
     ... (ft-template (first lft))
     ... (lft-template (rest lft))]))
 
-done!
 
-Lets write a function that determines if there is a blue-eyed descendant:
+;; write a function that determines if there is a blue-eyed descendant:
 
 ;; b-e-d? : family-tree -> boolean
+;; to determine if there is a blue-eyed descendent in a family tree
+
+;; examples (as tests):
 (check-expect (b-e-d? gustav) #false)
 (check-expect (b-e-d? carl) #true)
+
 (define (b-e-d? ft)
   (or (equal? "blue" (parent-eyes ft))
-      (b-d-e-l? (parent-children ft))))
+      (b-e-d-l? (parent-children ft))))
 
 ;; b-e-d-l? : family-tree-list -> boolean
+;; to determine if there is a blue-eyed descendent in a list of family trees
+
+;; examples (as tests)
 (check-expect (b-e-d-l? '()) #false)
 (check-expect (b-e-d-l? (list eva)) #true)
+
 (define (b-e-d-l? lft)
   (cond
    [(empty? lft) #false]
@@ -96,19 +111,29 @@ Lets write a function that determines if there is a blue-eyed descendant:
         (b-e-d-l? (rest lft)))]))
 
 
-(b-e-d? carl) =
-(b-e-d-l? (list adam dave eva fred))
-(or (b-e-d? adam) (b-e-d-l? (list dave eva fred)))
-(or (b-e-d-l? '()) (b-e-d-l? (list dave eva fred)))
-(b-e-d-l? (list dave eva fred))
-...
-true
+#|
+Lets try some hand-evaluation:
+|#
 
+(b-e-d? carl)
+"="
+(b-e-d-l? (list adam dave eva fred))
+"="
+(or (b-e-d? adam) (b-e-d-l? (list dave eva fred)))
+"="
+(or (b-e-d-l? '()) (b-e-d-l? (list dave eva fred)))
+"="
+(b-e-d-l? (list dave eva fred))
+"= ..."
+#true
+
+#|
 ============================================================
 
-If time, write:
+How about a function that computes the average age of
+the people in a family tree?
 
-  average-age : family-tree -> number
+Try to do this on your own for practice. HINT: you can
+re-use the `average` function from your homework
 
-Can we write this by re-using the average function from the homework?
-
+|#
