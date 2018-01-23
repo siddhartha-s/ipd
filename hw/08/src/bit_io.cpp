c5fe0373e5fb8bc6a672428df70a8c38ec5b57ff
Jesse A. Tov <jesse.tov@gmail.com>
2018-01-22 18:47:24 -0600

    Unindented inside ipd namespace.

--- hw/08/src/bit_io.cpp
+++ hw/08/src/bit_io.cpp
@@ -3,136 +3,137 @@
 #include <ios>
 
 namespace ipd {
-    using std::ios;
 
-    bistream &bistream::read(bool &bit) {
-        if (nbits == 0) {
-            if (!next_byte(bitbuf)) return *this;
-            nbits = 8;
-        }
+using std::ios;
 
-        --nbits;
-        bit = (bitbuf >> nbits & 1) == 1;
-
-        return *this;
+bistream &bistream::read(bool &bit) {
+    if (nbits == 0) {
+        if (!next_byte(bitbuf)) return *this;
+        nbits = 8;
     }
 
-    bistream::operator bool() const {
-        return good();
-    }
+    --nbits;
+    bit = (bitbuf >> nbits & 1) == 1;
 
-    bistream_adaptor::bistream_adaptor(std::istream& is) : stream_(is)
-    { }
+    return *this;
+}
 
-    bool bistream_adaptor::next_byte(char &bitbuf) {
-        return (bool) stream_.read(&bitbuf, 1);
-    }
+bistream::operator bool() const {
+    return good();
+}
 
-    bool bistream_adaptor::good() const {
-        return nbits > 0 || stream_.good();
-    }
+bistream_adaptor::bistream_adaptor(std::istream& is) : stream_(is)
+{ }
 
-    bool bistream_adaptor::eof() const {
-        return nbits == 0 && stream_.eof();
-    }
+bool bistream_adaptor::next_byte(char &bitbuf) {
+    return (bool) stream_.read(&bitbuf, 1);
+}
 
-    bistringstream::bistringstream(std::vector<unsigned char> v)
-            : bytes(v), bytes_index(0) {}
+bool bistream_adaptor::good() const {
+    return nbits > 0 || stream_.good();
+}
 
-    static std::vector<unsigned char>
-    bits_to_chars(std::initializer_list<bool> bits)
-    {
-        bostringstream oss;
-        for (bool bit : bits) oss << bit;
-        return oss.data();
-    }
+bool bistream_adaptor::eof() const {
+    return nbits == 0 && stream_.eof();
+}
 
-    bistringstream::bistringstream(std::initializer_list<bool> bits)
-            : bistringstream(bits_to_chars(bits))
-    { }
+bistringstream::bistringstream(std::vector<unsigned char> v)
+        : bytes(v), bytes_index(0) {}
 
-    bool bistringstream::next_byte(char &bitbuf) {
-        if (bytes_index < bytes.size()) {
-            bitbuf = bytes[bytes_index];
-            bytes_index++;
-            return true;
-        }
-        bitbuf = 0;
-        return false;
-    }
+static std::vector<unsigned char>
+bits_to_chars(std::initializer_list<bool> bits)
+{
+    bostringstream oss;
+    for (bool bit : bits) oss << bit;
+    return oss.data();
+}
 
-    bool bistringstream::good() const {
-        return nbits > 0 || bytes_index < bytes.size();
-    }
+bistringstream::bistringstream(std::initializer_list<bool> bits)
+        : bistringstream(bits_to_chars(bits))
+{ }
 
-    size_t bostringstream::bits_written() const
-    {
-        return bits_written_;
-    }
-
-    bool bistringstream::eof() const {
-        return nbits == 0 && bytes_index == bytes.size();
-    }
-
-    bifstream::bifstream(const char *filespec)
-            : stream(filespec, ios::binary)
-            , bistream_adaptor(stream)
-    {}
-
-    bostream_adaptor::bostream_adaptor(std::ostream& os)
-            : stream_(os)
-    {}
-
-    bostream_adaptor &bostream_adaptor::write(bool bit) {
-        bitbuf |= ((char) bit) << (7 - nbits);
-
-        if (++nbits == 8) write_out();
-
-        return *this;
-    }
-
-    bool bostream_adaptor::good() const {
-        return stream_.good();
-    }
-
-    void bostream_adaptor::write_out() {
-        if (stream_.write(&bitbuf, 1)) {
-            bitbuf = 0;
-            nbits = 0;
-        }
-    }
-
-    bostream_adaptor::~bostream_adaptor() {
-        if (nbits) {
-            write_out();
-        }
-    }
-
-    bostream::operator bool() const {
-        return good();
-    }
-
-    bofstream::bofstream(const char *filespec)
-            : stream(filespec, ios::binary | ios::trunc)
-            , bostream_adaptor(stream)
-    {}
-
-    bool bostringstream::good() const {
+bool bistringstream::next_byte(char &bitbuf) {
+    if (bytes_index < bytes.size()) {
+        bitbuf = bytes[bytes_index];
+        bytes_index++;
         return true;
     }
+    bitbuf = 0;
+    return false;
+}
 
-    bostringstream &bostringstream::write(bool bit) {
-        auto index = bits_written_ / 8;
-        auto nbits = bits_written_ % 8;
-        if (index >= data_.size()) data_.push_back(0);
-        data_[index] |= ((unsigned char) bit) << (7 - nbits);
-        bits_written_++;
-        return *this;
-    }
+bool bistringstream::good() const {
+    return nbits > 0 || bytes_index < bytes.size();
+}
 
-    const std::vector<unsigned char>& bostringstream::data() const {
-        return data_;
+size_t bostringstream::bits_written() const
+{
+    return bits_written_;
+}
+
+bool bistringstream::eof() const {
+    return nbits == 0 && bytes_index == bytes.size();
+}
+
+bifstream::bifstream(const char *filespec)
+        : stream(filespec, ios::binary)
+        , bistream_adaptor(stream)
+{}
+
+bostream_adaptor::bostream_adaptor(std::ostream& os)
+        : stream_(os)
+{}
+
+bostream_adaptor &bostream_adaptor::write(bool bit) {
+    bitbuf |= ((char) bit) << (7 - nbits);
+
+    if (++nbits == 8) write_out();
+
+    return *this;
+}
+
+bool bostream_adaptor::good() const {
+    return stream_.good();
+}
+
+void bostream_adaptor::write_out() {
+    if (stream_.write(&bitbuf, 1)) {
+        bitbuf = 0;
+        nbits = 0;
     }
+}
+
+bostream_adaptor::~bostream_adaptor() {
+    if (nbits) {
+        write_out();
+    }
+}
+
+bostream::operator bool() const {
+    return good();
+}
+
+bofstream::bofstream(const char *filespec)
+        : stream(filespec, ios::binary | ios::trunc)
+        , bostream_adaptor(stream)
+{}
+
+bool bostringstream::good() const {
+    return true;
+}
+
+bostringstream &bostringstream::write(bool bit) {
+    auto index = bits_written_ / 8;
+    auto nbits = bits_written_ % 8;
+    if (index >= data_.size()) data_.push_back(0);
+    data_[index] |= ((unsigned char) bit) << (7 - nbits);
+    bits_written_++;
+    return *this;
+}
+
+const std::vector<unsigned char>& bostringstream::data() const {
+    return data_;
+}
 
 }
 
--- hw/08/src/bit_io.h
+++ hw/08/src/bit_io.h
@@ -17,388 +17,389 @@
 #include <initializer_list>
 
 namespace ipd {
-    /*
-     * INPUT
-     */
+/*
+ * INPUT
+ */
 
-    // Bit input stream, for reading individual bits from a file or a std::vector<unsigned char>
-    class bistream {
-    public:
-        // Reads a bit from this bit input stream into the given bool
-        // reference.
-        //
-        // Parameters:
-        //
-        //      result - bool reference to store the bit that was read
-        //
-        // Returns: the same bit input stream, for method chaining
-        //
-        // Example:
-        //
-        //      bool b;
-        //      bif.read(b);
-        //
-        bistream &read(bool &result);
-
-        // Reads `n` bits, interpreted as a big-endian `n`-bit integer,
-        // and stores them in the reference `result`, which must have a
-        // numeric type.
-        //
-        // Parameters:
-        //
-        //      <typename T> - the type of the result; must be numeric
-        //      result - reference for storing the result
-        //      n - number of bits to read
-        //
-        // Returns: the same bit input stream, for method chaining
-        //
-        // Example:
-        //
-        //      int result;
-        //      bif.read_bits(result, 5); // reads 5 bits into result
-        //
-        template<typename T>
-        bistream &read_bits(T &result, size_t n);
-
-        // Determines whether we've reached the end of the input file.
-        //
-        // Returns: `true` if we’ve attempted to read past the end of
-        // the file, and `false` otherwise.
-        //
-        // Example:
-        //
-        //      while (bif.read(b) && !bif.eof()) {
-        //          ...
-        //      }
-        //
-        virtual bool eof() const = 0;
-
-        // Determines the status of the bit input stream.
-        //
-        // Returns: `false` if there are no bits to read and a read
-        // error has occurred, and `true` otherwise.
-        //
-        // Example:
-        //
-        //      bif.read(b);
-        //      if (bif.good()) {
-        //          // we know the read succeeded
-        //      }
-        //
-        virtual bool good() const = 0;
-
-        // The bit input stream boolean coercion operator, inserted, for
-        // example, when a `bifstream` is used as a condition for an
-        // `if`. Alias for `good()`.
-        //
-        // Returns: `false` if there are no bits to read and a read
-        // error has occurred, and `true` otherwise.
-        //
-        // Example:
-        //
-        //      bif.read(b);
-        //      if (bif) {
-        //          // we know the read succeeded
-        //      }
-        //
-        operator bool() const;
-
-    protected:
-
-        virtual bool next_byte(char &) = 0;
-
-        short nbits = 0;
-
-    private:
-
-        char bitbuf = 0;
-
-    };
-
-    // Adapts any `std::istream&` for use as a bit input stream.
-    class bistream_adaptor : public bistream {
-    public:
-        // Constructs a bit input stream on top of an input stream. Does not
-        // take ownership of the `std::istream`. However, using the `istream`
-        // while also using the adaptor, or adapting the same `istream`
-        // twice, will give strange results.
-        //
-        // Parameters:
-        //
-        //      input_stream - the input stream to adapt
-        //
-        // Example:
-        //
-        //      bistream_adaptor bis(input_stream);
-        //
-        explicit bistream_adaptor(std::istream&);
-
-        bool eof() const override;
-
-        bool good() const override;
-
-        bistream_adaptor(const bistream_adaptor &) = delete;
-
-    private:
-        std::istream& stream_;
-
-        virtual bool next_byte(char &) override;
-
-    };
-
-    class bifstream : public bistream_adaptor {
-    public:
-        // Constructs a bit input stream to read from the given file.
-        //
-        // Parameters:
-        //
-        //      filespec - name of the file to open
-        //
-        // Example:
-        //
-        //      bifstream bif(input_file_name);
-        //
-        explicit bifstream(const char *filespec);
-
-        bifstream(const bifstream &) = delete;
-
-    private:
-        std::ifstream stream;
-    };
-
-    class bistringstream : public bistream {
-    public:
-        // Constructs a bit input stream to read from the given file.
-        //
-        // Parameters:
-        //
-        //      filespec - name of the file to open
-        //
-        // Example:
-        //
-        //      bifstream bif(input_file_name);
-        //
-        explicit bistringstream(std::vector<unsigned char>);
-
-        // Creates a bit input stream containing exactly the given bits.
-        explicit bistringstream(std::initializer_list<bool>);
-
-        bool eof() const override;
-
-        bool good() const override;
-
-        bistringstream(const bistringstream &) = delete;
-
-    private:
-        size_t bytes_index;
-        std::vector<unsigned char> bytes;
-        size_t total_bits;
-
-        virtual bool next_byte(char &) override;
-
-    };
-
-    // The bit input stream extraction operator; alias for `read(bool&)`.
+// Bit input stream, for reading individual bits from a file or a std::vector<unsigned char>
+class bistream {
+public:
+    // Reads a bit from this bit input stream into the given bool
+    // reference.
     //
-    // Params:
+    // Parameters:
     //
-    //     - bif - the bit input stream to read from
-    //     - b - reference to store the bit that was read
+    //      result - bool reference to store the bit that was read
     //
     // Returns: the same bit input stream, for method chaining
     //
     // Example:
     //
-    //     bool b1, b2, b3;
-    //     bif >> b1 >> b2 >> b3;
+    //      bool b;
+    //      bif.read(b);
     //
-    bistream &operator>>(bistream &bif, bool &b);
+    bistream &read(bool &result);
 
-    /*
-     * OUTPUT
-     */
-
-    // A bit output stream, for writing individual bits to a file.
-    class bostream {
-    public:
-        // Writes a bit to this bit output stream.
-        //
-        // Parameters:
-        //
-        //      b - the bit to write
-        //
-        // Returns: the same bit output stream, for method chaining
-        //
-        // Example:
-        //
-        //      bof.write(true);
-        //
-        virtual bostream &write(bool b) = 0;
-
-        // Writes an `n`-bit big-endian representation of `value`, which
-        // must have a numeric type, to this bit output sream.
-        //
-        // Parameters:
-        //
-        //      <typename T> - the type of the value to write; must be numeric
-        //      value - the value to write
-        //      n - number of bits to write
-        //
-        // Returns: the same bit output stream, for method chaining
-        //
-        // Example:
-        //
-        //      bof.write(22, 5); // writes 10110
-        //      bof.write(22, 6); // writes 010110
-        //
-        template<typename T>
-        bostream &write_bits(T value, size_t n);
-
-        // Determines the status of the bit output stream.
-        //
-        // Returns: `false` if an error has occurred; `true` otherwise
-        //
-        // Example:
-        //
-        //      bof.write(b);
-        //      if (bof.good()) {
-        //          // we know the write succeeded
-        //      }
-        //
-        virtual bool good() const = 0;
-
-        // The bit output stream boolean coercion operator, inserted, for
-        // example, when a `bofstream` is used as a condition for an
-        // `if`. Alias for `good()`.
-        //
-        // Returns: `false` if an error has occurred; `true` otherwise
-        //
-        // Example:
-        //
-        //      bof.write(b);
-        //      if (bof) {
-        //          // we know the write succeeded
-        //      }
-        //
-        operator bool() const;
-
-    };
-
-    // Adapts any `std::ostream&` for use as a bit output stream.
-    class bostream_adaptor : public bostream {
-    public:
-        // Constructs a bit output stream to write to the given output stream.
-        //
-        // Parameters:
-        //
-        //      output_stream - the byte output stream to adapt
-        //
-        // Example:
-        //
-        //      bostream_adaptor bos(output_stream);
-        //
-        explicit bostream_adaptor(std::ostream&);
-
-        virtual bostream_adaptor &write(bool b) override;
-
-        virtual bool good() const override;
-
-        ~bostream_adaptor();
-
-        bostream_adaptor(const bostream_adaptor &) = delete;
-
-    private:
-        char bitbuf;
-        short nbits;
-        std::ostream& stream_;
-
-        void write_out();
-    };
-
-    class bofstream : public bostream_adaptor {
-    public:
-        // Constructs a bit output stream to write to the given file.
-        //
-        // Parameters:
-        //
-        //      filespec - name of the file to open or create
-        //
-        // Example:
-        //
-        //      bofstream bof(output_file_name);
-        //
-        explicit bofstream(const char *filespec);
-
-        bofstream(const bofstream &) = delete;
-
-    private:
-        std::ofstream stream;
-    };
-
-    class bostringstream : public bostream {
-    public:
-        const std::vector<unsigned char>& data() const;
-
-        virtual bostringstream &write(bool b) override;
-
-        virtual bool good() const override;
-
-        size_t bits_written() const;
-
-    private:
-        size_t bits_written_ = 0;
-        std::vector<unsigned char> data_;
-
-    };
-
-    // The bit output stream insertion operator; alias for
-    // `write(bool)`.
+    // Reads `n` bits, interpreted as a big-endian `n`-bit integer,
+    // and stores them in the reference `result`, which must have a
+    // numeric type.
     //
-    // Params:
+    // Parameters:
     //
-    //     - bof - the bit input stream to write to
-    //     - b - the bit to write
+    //      <typename T> - the type of the result; must be numeric
+    //      result - reference for storing the result
+    //      n - number of bits to read
+    //
+    // Returns: the same bit input stream, for method chaining
+    //
+    // Example:
+    //
+    //      int result;
+    //      bif.read_bits(result, 5); // reads 5 bits into result
+    //
+    template<typename T>
+    bistream &read_bits(T &result, size_t n);
+
+    // Determines whether we've reached the end of the input file.
+    //
+    // Returns: `true` if we’ve attempted to read past the end of
+    // the file, and `false` otherwise.
+    //
+    // Example:
+    //
+    //      while (bif.read(b) && !bif.eof()) {
+    //          ...
+    //      }
+    //
+    virtual bool eof() const = 0;
+
+    // Determines the status of the bit input stream.
+    //
+    // Returns: `false` if there are no bits to read and a read
+    // error has occurred, and `true` otherwise.
+    //
+    // Example:
+    //
+    //      bif.read(b);
+    //      if (bif.good()) {
+    //          // we know the read succeeded
+    //      }
+    //
+    virtual bool good() const = 0;
+
+    // The bit input stream boolean coercion operator, inserted, for
+    // example, when a `bifstream` is used as a condition for an
+    // `if`. Alias for `good()`.
+    //
+    // Returns: `false` if there are no bits to read and a read
+    // error has occurred, and `true` otherwise.
+    //
+    // Example:
+    //
+    //      bif.read(b);
+    //      if (bif) {
+    //          // we know the read succeeded
+    //      }
+    //
+    operator bool() const;
+
+protected:
+
+    virtual bool next_byte(char &) = 0;
+
+    short nbits = 0;
+
+private:
+
+    char bitbuf = 0;
+
+};
+
+// Adapts any `std::istream&` for use as a bit input stream.
+class bistream_adaptor : public bistream {
+public:
+    // Constructs a bit input stream on top of an input stream. Does not
+    // take ownership of the `std::istream`. However, using the `istream`
+    // while also using the adaptor, or adapting the same `istream`
+    // twice, will give strange results.
+    //
+    // Parameters:
+    //
+    //      input_stream - the input stream to adapt
+    //
+    // Example:
+    //
+    //      bistream_adaptor bis(input_stream);
+    //
+    explicit bistream_adaptor(std::istream&);
+
+    bool eof() const override;
+
+    bool good() const override;
+
+    bistream_adaptor(const bistream_adaptor &) = delete;
+
+private:
+    std::istream& stream_;
+
+    virtual bool next_byte(char &) override;
+
+};
+
+class bifstream : public bistream_adaptor {
+public:
+    // Constructs a bit input stream to read from the given file.
+    //
+    // Parameters:
+    //
+    //      filespec - name of the file to open
+    //
+    // Example:
+    //
+    //      bifstream bif(input_file_name);
+    //
+    explicit bifstream(const char *filespec);
+
+    bifstream(const bifstream &) = delete;
+
+private:
+    std::ifstream stream;
+};
+
+class bistringstream : public bistream {
+public:
+    // Constructs a bit input stream to read from the given file.
+    //
+    // Parameters:
+    //
+    //      filespec - name of the file to open
+    //
+    // Example:
+    //
+    //      bifstream bif(input_file_name);
+    //
+    explicit bistringstream(std::vector<unsigned char>);
+
+    // Creates a bit input stream containing exactly the given bits.
+    explicit bistringstream(std::initializer_list<bool>);
+
+    bool eof() const override;
+
+    bool good() const override;
+
+    bistringstream(const bistringstream &) = delete;
+
+private:
+    size_t bytes_index;
+    std::vector<unsigned char> bytes;
+    size_t total_bits;
+
+    virtual bool next_byte(char &) override;
+
+};
+
+// The bit input stream extraction operator; alias for `read(bool&)`.
+//
+// Params:
+//
+//     - bif - the bit input stream to read from
+//     - b - reference to store the bit that was read
+//
+// Returns: the same bit input stream, for method chaining
+//
+// Example:
+//
+//     bool b1, b2, b3;
+//     bif >> b1 >> b2 >> b3;
+//
+bistream &operator>>(bistream &bif, bool &b);
+
+/*
+ * OUTPUT
+ */
+
+// A bit output stream, for writing individual bits to a file.
+class bostream {
+public:
+    // Writes a bit to this bit output stream.
+    //
+    // Parameters:
+    //
+    //      b - the bit to write
     //
     // Returns: the same bit output stream, for method chaining
     //
     // Example:
     //
-    //     bof << true << false << false;
+    //      bof.write(true);
     //
-    bofstream &operator<<(bofstream &bof, bool b);
+    virtual bostream &write(bool b) = 0;
 
-    /*
-     * TEMPLATE IMPLEMENTATIONS
-     */
-
+    // Writes an `n`-bit big-endian representation of `value`, which
+    // must have a numeric type, to this bit output sream.
+    //
+    // Parameters:
+    //
+    //      <typename T> - the type of the value to write; must be numeric
+    //      value - the value to write
+    //      n - number of bits to write
+    //
+    // Returns: the same bit output stream, for method chaining
+    //
+    // Example:
+    //
+    //      bof.write(22, 5); // writes 10110
+    //      bof.write(22, 6); // writes 010110
+    //
     template<typename T>
-    bistream &bistream::read_bits(T &result, size_t n) {
-        bool bit;
+    bostream &write_bits(T value, size_t n);
 
-        result = 0;
+    // Determines the status of the bit output stream.
+    //
+    // Returns: `false` if an error has occurred; `true` otherwise
+    //
+    // Example:
+    //
+    //      bof.write(b);
+    //      if (bof.good()) {
+    //          // we know the write succeeded
+    //      }
+    //
+    virtual bool good() const = 0;
 
-        while (n--) {
-            read(bit);
-            result = result << 1 | bit;
-        }
+    // The bit output stream boolean coercion operator, inserted, for
+    // example, when a `bofstream` is used as a condition for an
+    // `if`. Alias for `good()`.
+    //
+    // Returns: `false` if an error has occurred; `true` otherwise
+    //
+    // Example:
+    //
+    //      bof.write(b);
+    //      if (bof) {
+    //          // we know the write succeeded
+    //      }
+    //
+    operator bool() const;
 
-        return *this;
+};
+
+// Adapts any `std::ostream&` for use as a bit output stream.
+class bostream_adaptor : public bostream {
+public:
+    // Constructs a bit output stream to write to the given output stream.
+    //
+    // Parameters:
+    //
+    //      output_stream - the byte output stream to adapt
+    //
+    // Example:
+    //
+    //      bostream_adaptor bos(output_stream);
+    //
+    explicit bostream_adaptor(std::ostream&);
+
+    virtual bostream_adaptor &write(bool b) override;
+
+    virtual bool good() const override;
+
+    ~bostream_adaptor();
+
+    bostream_adaptor(const bostream_adaptor &) = delete;
+
+private:
+    char bitbuf;
+    short nbits;
+    std::ostream& stream_;
+
+    void write_out();
+};
+
+class bofstream : public bostream_adaptor {
+public:
+    // Constructs a bit output stream to write to the given file.
+    //
+    // Parameters:
+    //
+    //      filespec - name of the file to open or create
+    //
+    // Example:
+    //
+    //      bofstream bof(output_file_name);
+    //
+    explicit bofstream(const char *filespec);
+
+    bofstream(const bofstream &) = delete;
+
+private:
+    std::ofstream stream;
+};
+
+class bostringstream : public bostream {
+public:
+    const std::vector<unsigned char>& data() const;
+
+    virtual bostringstream &write(bool b) override;
+
+    virtual bool good() const override;
+
+    size_t bits_written() const;
+
+private:
+    size_t bits_written_ = 0;
+    std::vector<unsigned char> data_;
+
+};
+
+// The bit output stream insertion operator; alias for
+// `write(bool)`.
+//
+// Params:
+//
+//     - bof - the bit input stream to write to
+//     - b - the bit to write
+//
+// Returns: the same bit output stream, for method chaining
+//
+// Example:
+//
+//     bof << true << false << false;
+//
+bofstream &operator<<(bofstream &bof, bool b);
+
+/*
+ * TEMPLATE IMPLEMENTATIONS
+ */
+
+template<typename T>
+bistream &bistream::read_bits(T &result, size_t n) {
+    bool bit;
+
+    result = 0;
+
+    while (n--) {
+        read(bit);
+        result = result << 1 | bit;
     }
 
-    inline bistream &operator>>(bistream &bif, bool &bit) {
-        return bif.read(bit);
+    return *this;
+}
+
+inline bistream &operator>>(bistream &bif, bool &bit) {
+    return bif.read(bit);
+}
+
+template<typename T>
+bostream &bostream::write_bits(T value, size_t n) {
+    while (n--) {
+        write(value >> n & 1);
     }
 
-    template<typename T>
-    bostream &bostream::write_bits(T value, size_t n) {
-        while (n--) {
-            write(value >> n & 1);
-        }
+    return *this;
+}
 
-        return *this;
-    }
+inline bostream &operator<<(bostream &bof, bool bit) {
+    return bof.write(bit);
+}
 
-    inline bostream &operator<<(bostream &bof, bool bit) {
-        return bof.write(bit);
-    }
 }
